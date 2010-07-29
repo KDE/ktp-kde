885da92a311ecefbb327f61f44c5d42a4399fa9a
Daniele Elmo Domenichelli <daniele.domenichelli@gmail.com>
2010-07-29 22:13:54 +0000

    Added ModelMenu class
    Original files from Qt 4.6.2:
      /demos/browser/modelmenu.h
      /demos/browser/modelmenu.cpp
    
    svn path=/trunk/playground/network/libktelepathy/; revision=1156869

---
+++ src/KTelepathy/modelmenu.h
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2010 Nokia Corporation <qt-info@nokia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef MODELMENU_H
+#define MODELMENU_H
+
+#include <QtGui/QMenu>
+#include <QtCore/QAbstractItemModel>
+
+// A QMenu that is dynamically populated from a QAbstractItemModel
+class ModelMenu : public QMenu
+{
+    Q_OBJECT
+
+signals:
+    void activated(const QModelIndex &index);
+    void hovered(const QString &text);
+
+public:
+    ModelMenu(QWidget *parent = 0);
+
+    void setModel(QAbstractItemModel *model);
+    QAbstractItemModel *model() const;
+
+    void setMaxRows(int max);
+    int maxRows() const;
+
+    void setFirstSeparator(int offset);
+    int firstSeparator() const;
+
+    void setRootIndex(const QModelIndex &index);
+    QModelIndex rootIndex() const;
+
+    void setHoverRole(int role);
+    int hoverRole() const;
+
+    void setSeparatorRole(int role);
+    int separatorRole() const;
+
+    QAction *makeAction(const QIcon &icon, const QString &text, QObject *parent);
+
+protected:
+    // add any actions before the tree, return true if any actions are added.
+    virtual bool prePopulated();
+    // add any actions after the tree
+    virtual void postPopulated();
+    // put all of the children of parent into menu up to max
+    void createMenu(const QModelIndex &parent, int max, QMenu *parentMenu = 0, QMenu *menu = 0);
+
+private slots:
+    void aboutToShow();
+    void triggered(QAction *action);
+    void hovered(QAction *action);
+
+private:
+    QAction *makeAction(const QModelIndex &index);
+    int m_maxRows;
+    int m_firstSeparator;
+    int m_maxWidth;
+    int m_hoverRole;
+    int m_separatorRole;
+    QAbstractItemModel *m_model;
+    QPersistentModelIndex m_root;
+};
+
+#endif // MODELMENU_H
+
