2d011211919ae9a9dae68b71efca39e36e84942e
Daniele Elmo Domenichelli <daniele.domenichelli@gmail.com>
2010-08-06 20:48:58 +0000

    Added RequestP2PDBusTubeJob
    svn path=/trunk/playground/network/libktelepathy/; revision=1160034

---
+++ src/jobs/requestp2pdbustubejob.cpp
@@ -0,0 +1,189 @@
+/*
+* Copyright (C) 2010 Daniele E. Domenichelli <daniele.domenichelli@gmail.com>
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "requestp2pdbustubejob.h"
+#include "abstractrequestchanneljob_p.h"
+
+#include <TelepathyQt4/Account>
+#include <KDebug>
+#include "ontologies/imaccount.h"
+
+class RequestP2PDBusTubeJobPrivate : public AbstractRequestChannelJobPrivate
+{
+    Q_DECLARE_PUBLIC(RequestP2PDBusTubeJob)
+
+public:
+    RequestP2PDBusTubeJobPrivate(const Nepomuk::PersonContact& c,
+                                 const QString& sn,
+                                 const QString& ph,
+                                 const KTelepathy::RequestChannelFlags requestchannelflags);
+    RequestP2PDBusTubeJobPrivate(const Nepomuk::Person& mc,
+                                 const QString& sn,
+                                 const QString& ph,
+                                 const KTelepathy::RequestChannelFlags requestchannelflags);
+    ~RequestP2PDBusTubeJobPrivate();
+
+    QString serviceName;
+
+    static inline RequestP2PDBusTubeJob* newJob(const Nepomuk::PersonContact& contact,
+                                                const QString& serviceName,
+                                                const QString& preferredHandler,
+                                                const KTelepathy::RequestChannelFlags requestchannelflags,
+                                                QObject* parent)
+    {
+        kDebug() << contact.genericLabel();
+        kDebug() << serviceName;
+        kDebug() << preferredHandler;
+        kDebug() << requestchannelflags;
+        RequestP2PDBusTubeJob *job = new RequestP2PDBusTubeJob(*new RequestP2PDBusTubeJobPrivate(contact,
+                                                                                                 serviceName,
+                                                                                                 preferredHandler,
+                                                                                                 requestchannelflags),
+                                                               parent);
+        return job;
+    }
+    static inline RequestP2PDBusTubeJob* newJob(const Nepomuk::Person& metacontact,
+                                                const QString& serviceName,
+                                                const QString& preferredHandler,
+                                                const KTelepathy::RequestChannelFlags requestchannelflags,
+                                                QObject* parent)
+    {
+        kDebug() << metacontact.genericLabel();
+        kDebug() << serviceName;
+        kDebug() << preferredHandler;
+        kDebug() << requestchannelflags;
+        RequestP2PDBusTubeJob *job = new RequestP2PDBusTubeJob(*new RequestP2PDBusTubeJobPrivate(metacontact,
+                                                                                                 serviceName,
+                                                                                                 preferredHandler,
+                                                                                                 requestchannelflags),
+                                                               parent);
+        return job;
+    }
+};
+
+
+RequestP2PDBusTubeJob::RequestP2PDBusTubeJob(RequestP2PDBusTubeJobPrivate &dd, QObject *parent)
+    : AbstractRequestChannelJob(dd, parent)
+{
+}
+
+
+RequestP2PDBusTubeJob::~RequestP2PDBusTubeJob()
+{
+}
+
+
+Tp::PendingChannelRequest* RequestP2PDBusTubeJob::createChannel()
+{
+    Q_D(RequestP2PDBusTubeJob);
+    kDebug() << d->contactResource.resourceUri() << d->contact->id();
+    kDebug() << d->serviceName;
+    kDebug() << d->useractiontime;
+    kDebug() << d->preferredHandler;
+    return d->account->createP2PDBusTube(d->contact, d->serviceName, d->useractiontime, d->preferredHandler);
+}
+
+
+bool RequestP2PDBusTubeJob::canCreateChannel()
+{
+    return true;
+}
+
+
+RequestP2PDBusTubeJobPrivate::RequestP2PDBusTubeJobPrivate(const Nepomuk::PersonContact& c,
+                                                           const QString& sn,
+                                                           const QString& ph,
+                                                           const KTelepathy::RequestChannelFlags requestchannelflags)
+    : AbstractRequestChannelJobPrivate(c, ph, requestchannelflags),
+      serviceName(sn)
+{
+    kDebug();
+}
+
+
+RequestP2PDBusTubeJobPrivate::RequestP2PDBusTubeJobPrivate(const Nepomuk::Person& mc,
+                                                           const QString& sn,
+                                                           const QString& ph,
+                                                           const KTelepathy::RequestChannelFlags requestchannelflags)
+    : AbstractRequestChannelJobPrivate(mc, ph, requestchannelflags),
+      serviceName(sn)
+{
+    kDebug();
+}
+
+
+RequestP2PDBusTubeJobPrivate::~RequestP2PDBusTubeJobPrivate()
+{
+    kDebug();
+}
+
+/*
+RequestP2PDBusTubeJob* requestP2PDBusTube(const Nepomuk::PersonContact& contact,
+                                          const QString& serviceName,
+                                          const QString& preferredHandler,
+                                          const KTelepathy::RequestChannelFlags requestchannelflags,
+                                          QObject* parent)
+{
+    return RequestP2PDBusTubeJobPrivate::newJob(contact,
+                                                serviceName,
+                                                preferredHandler,
+                                                requestchannelflags,
+                                                parent);
+
+}
+
+RequestP2PDBusTubeJob* requestP2PDBusTube(const Nepomuk::Person& metacontact,
+                                    const QString preferredHandler,
+                                    const KTelepathy::RequestChannelFlags requestchannelflags,
+                                    QObject* parent)
+{
+    return RequestP2PDBusTubeJobPrivate::newJob(metacontact,
+                                             preferredHandler,
+                                             requestchannelflags,
+                                             parent);
+}
+*/
+
+RequestP2PDBusTubeJob* requestP2PDBusTube(const Nepomuk::PersonContact& contact,
+                                          const QString& serviceName,
+                                          const QString& preferredHandler,
+                                          QObject* parent)
+{
+    kDebug();
+    return RequestP2PDBusTubeJobPrivate::newJob(contact,
+                                                serviceName,
+                                                preferredHandler,
+                                                KTelepathy::RequestChannelCreateMode,
+                                                parent);
+
+}
+
+RequestP2PDBusTubeJob* requestP2PDBusTube(const Nepomuk::Person& metacontact,
+                                          const QString& serviceName,
+                                          const QString& preferredHandler,
+                                          QObject* parent)
+{
+    kDebug();
+    return RequestP2PDBusTubeJobPrivate::newJob(metacontact,
+                                                serviceName,
+                                                preferredHandler,
+                                                KTelepathy::RequestChannelCreateMode,
+                                                parent);
+}
+
+#include "requestp2pdbustubejob.moc"
---
+++ src/jobs/requestp2pdbustubejob.h
@@ -0,0 +1,68 @@
+/*
+* Copyright (C) 2010 Daniele E. Domenichelli <daniele.domenichelli@gmail.com>
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef LIBKTELEPATHY_REQUESTP2PDBUSTUBEJOB_H
+#define LIBKTELEPATHY_REQUESTP2PDBUSTUBEJOB_H
+
+
+#include "abstractrequestchanneljob.h"
+#include <kdemacros.h>
+
+class RequestP2PDBusTubeJobPrivate;
+class KDE_EXPORT RequestP2PDBusTubeJob : public AbstractRequestChannelJob
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(RequestP2PDBusTubeJob)
+    Q_DECLARE_PRIVATE(RequestP2PDBusTubeJob)
+
+public:
+    RequestP2PDBusTubeJob(RequestP2PDBusTubeJobPrivate &dd, QObject *parent = 0);
+    virtual ~RequestP2PDBusTubeJob();
+
+    virtual Tp::PendingChannelRequest* createChannel();
+    virtual bool canCreateChannel();
+};
+
+
+namespace Nepomuk {
+    class PersonContact;
+    class Person;
+}
+
+
+/*
+KDE_EXPORT RequestP2PDBusTubeJob* requestP2PDBusTube(const Nepomuk::PersonContact& contact,
+                                                     const QString& serviceName,
+                                                     const QString& preferredHandler = QString(),
+                                                     const RequestChannelJob::RequestFlags requestchannelflags = RequestChannelJob::RequestModeEnsure,
+                                                     QObject* parent = 0);
+KDE_EXPORT RequestP2PDBusTubeJob* requestP2PDBusTube(const Nepomuk::Person& metacontact,
+                                                     const QString& serviceName,
+                                                     const QString& preferredHandler = QString(),
+                                                     const RequestChannelJob::RequestFlags requestchannelflags = RequestChannelJob::RequestModeEnsure,
+                                                     QObject* parent = 0);
+*/
+KDE_EXPORT RequestP2PDBusTubeJob* requestP2PDBusTube(const Nepomuk::PersonContact& contact,
+                                                     const QString& serviceName,
+                                                     const QString& preferredHandler,
+                                                     QObject* parent);
+KDE_EXPORT RequestP2PDBusTubeJob* requestP2PDBusTube(const Nepomuk::Person& metacontact,
+                                                     const QString& serviceName,
+                                                     const QString& preferredHandler,
+                                                     QObject* parent);
+#endif // LIBKTELEPATHY_REQUESTP2PDBUSTUBEJOB_H
